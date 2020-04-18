27df51f5934c012843f23bf5264174d103eeee77
daniel coats <daniel.e.coats@gmail.com>
2020-04-18 15:48:21 -0700

    Created PlayerHand and Deck classes in Data Folder.

---
+++ CardNightFever/Data/Deck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardNightFever.Data
+{
+    public class Deck
+    {
+        public List<PlayingCard> Cards { get; set; }
+
+        public PlayingCard DrawCard()
+        {
+            PlayingCard target = Cards.First();
+            Cards.Remove(target);
+            return target;
+        }
+
+        public void ShuffleCards()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < Cards.Count; i++)
+            {
+                PlayingCard c = Cards[i];
+                Cards.Insert(random.Next(0, Cards.Count), c);
+                Cards.Remove(c);
+            }
+        }
+    }
+}
---
+++ CardNightFever/Data/PlayerHand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CardNightFever.Shared;
+
+namespace CardNightFever.Data
+{
+    public class PlayerHand
+    {
+        [Required]
+        public int PlayerId { get; set; }
+        protected List<PlayingCard> Cards { get; set; }
+
+        public bool NeedDraw()
+        {
+            return Cards.Count < 11;
+        }
+
+        protected void DrawCard(Deck deck)
+        {
+            if (NeedDraw())
+            {
+                Cards.Add(deck.DrawCard());
+            }
+        }
+
+        protected void DrawMultcards(Deck deck, int amount)
+        {
+            for (int i = amount; i < amount; i++)
+            {
+                if (NeedDraw())
+                {
+                    Cards.Add(deck.DrawCard());
+                }
+            }
+        }
+
+        protected void FillHand(Deck deck)
+        {
+            while (NeedDraw() == true)
+            {
+                Cards.Add(deck.DrawCard());
+            }
+        }
+    }
+}
