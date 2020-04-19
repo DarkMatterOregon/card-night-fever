e36e1dfb79498700d02f92a2f0095d9a9fc9b811
Mark Davis <github@nohorse.com>
2020-04-18 17:57:14 -0700

    gitflow-feature-stash: DragDrop

--- CardNightFever/Startup.cs
+++ CardNightFever/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazor.DragDrop.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +29,7 @@ namespace CardNightFever
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddBlazorDragDrop();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
